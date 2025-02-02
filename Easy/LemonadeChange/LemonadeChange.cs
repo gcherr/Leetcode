6f30799ef758b5e0df08595332b9b3a6c73ee9dd
gcherr <gcherr@users.noreply.github.com>
2025-02-02 01:48:09 -0800

    Adding LemonadeChange solution
    Testing Github Desktop

---
+++ Easy/LemonadeChange/LemonadeChange.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public bool LemonadeChange(int[] bills)
+    {
+        int[] denominations = [0, 0, 0];
+
+        for (int i = 0; i < bills.Length; i++)
+        {
+            if (bills[i] == 5)
+            {
+                denominations[0]++;
+            }
+            else if (bills[i] == 10)
+            {
+                if (denominations[0] == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    denominations[0]--;
+                    denominations[1]++;
+                }
+            }
+            else
+            {
+                if (denominations[1] > 0 && denominations[0] > 0)
+                {
+                    denominations[0]--;
+                    denominations[1]--;
+                }
+                else if (denominations[0] >= 3)
+                {
+                    denominations[0] -= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
